56ca96cb528fc08e95f56a86e74a8b53bb4c7519
Muhammed Qadan <qadan97219@gmail.com>
2020-06-17 01:19:57 -0700

    Updates

---
+++ precache-manifest.9cad7f70af41f3a76ce69ddbe78290bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6c9698255241b5560bd9dbcd545de8",
+    "url": "/TaboulehFinder/index.html"
+  },
+  {
+    "revision": "f36e6c4fe94bbc310ab6",
+    "url": "/TaboulehFinder/static/css/main.43af3c9b.chunk.css"
+  },
+  {
+    "revision": "e6ef27a3d9a18649b659",
+    "url": "/TaboulehFinder/static/js/2.dcefa08e.chunk.js"
+  },
+  {
+    "revision": "80670e6d2a9be228a467582c01960795",
+    "url": "/TaboulehFinder/static/js/2.dcefa08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36e6c4fe94bbc310ab6",
+    "url": "/TaboulehFinder/static/js/main.b5ccfe76.chunk.js"
+  },
+  {
+    "revision": "cbec8bcaf5aeb588f0ed",
+    "url": "/TaboulehFinder/static/js/runtime-main.85dd79d9.js"
+  },
+  {
+    "revision": "e32444726264e197038d4e56a63758f1",
+    "url": "/TaboulehFinder/static/media/call.e3244472.png"
+  },
+  {
+    "revision": "bdaa7b38c401ce20596055477a018022",
+    "url": "/TaboulehFinder/static/media/flower.bdaa7b38.png"
+  },
+  {
+    "revision": "178f64dff1b9523252798ea37fa8e914",
+    "url": "/TaboulehFinder/static/media/lemon.178f64df.png"
+  },
+  {
+    "revision": "d13e9f802c077f8d574c7e086b61084a",
+    "url": "/TaboulehFinder/static/media/location.d13e9f80.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TaboulehFinder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68f5a81d33c1b41d76eaab5ddf553ca4",
+    "url": "/TaboulehFinder/static/media/olive-oil.68f5a81d.png"
+  },
+  {
+    "revision": "953cc3764a4c7a4f1edbb64710c6f896",
+    "url": "/TaboulehFinder/static/media/parsley.953cc376.png"
+  },
+  {
+    "revision": "2a2466218cb9f3365384c1b42ea61f57",
+    "url": "/TaboulehFinder/static/media/salt-and-pepper.2a246621.png"
+  },
+  {
+    "revision": "2d8bfb5add5c07792bf0ded27dbe8d4e",
+    "url": "/TaboulehFinder/static/media/tabouleh.2d8bfb5a.png"
+  },
+  {
+    "revision": "c688b12f9d0574d7dd02aa986484b313",
+    "url": "/TaboulehFinder/static/media/tomato.c688b12f.png"
+  },
+  {
+    "revision": "ec7fe2d650cb55dfcdf1469583ca02c4",
+    "url": "/TaboulehFinder/static/media/wheat(1).ec7fe2d6.png"
+  },
+  {
+    "revision": "79fd11cae75c10c171d1a8216ceba946",
+    "url": "/TaboulehFinder/static/media/wheat.79fd11ca.png"
+  }
+]);
